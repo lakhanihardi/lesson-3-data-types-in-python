91c54330e8e7d04aa8a6afbfc8563fdf22fe2d00
lakhanihardi <hardi.jgd@gmail.com>
2025-02-21 09:51:47 +0530

    Data type casting

---
+++ Type casting.py
@@ -0,0 +1,16 @@
+name="rabbits"
+age=14
+is_student=True
+weight=36.8
+print("name :", name)
+print("Data Type of name is",type(name))
+print("Age :", age)
+print("Data Type of age is",type(age))
+print("is_student :", is_student)
+print("Data Type of is_student",type(is_student))
+print("weight :", weight)
+print("Data Type of weight is",type(weight))
+print("\n after Type Casting...")
+age=str(age)
+print(age)
+print("Data Type of age ",type(age))
